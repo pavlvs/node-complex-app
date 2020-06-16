09a1ed2caa4d44e3d1ff46523dd7a91bb25e86ac
Paul J Shaw <pavlvsxavier@gmail.com>
2020-06-16 11:09:01 +0100

    Set up sessions

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
-
-mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
-    module.exports = client.db()
+mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(
+    err,
+    client
+) {
+    module.exports = client
     const app = require('./app')
     app.listen(process.env.PORT)
 })
