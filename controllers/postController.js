07d2fe95d4f48cc82f1795c35483e2ff8058a2df
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-09-17 15:18:23 +0100

    added updte post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,3 +30,33 @@ exports.viewEditScreen = async function (req, res) {
         res.render('404')
     }
 }
+
+exports.edit = function (req, res) {
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status) => {
+        // the post was successfully updated in the database
+        // or user had permission but there were validation errors
+        if (status == 'success') {
+            // post updated in db
+            req.flash('success', 'Post sucessfully updated.')
+            req.session.save(function () {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        } else {
+            post.errors.forEach(error => {
+                req.flash('errors', error)
+
+            })
+            req.session.save(function () {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        }
+    }).catch(() => {
+        // a post with requested id does not exist
+        // or if the current visitor is not the owner of the post
+        req.flash('errors', 'You do not have permission to perform that action')
+        req.session.save(function () {
+            res.redirect('/')
+        })
+    })
+}
--- models/Post.js
+++ models/Post.js
@@ -3,10 +3,11 @@ const postsCollection = require('../db').db().collection('posts')
 const ObjectID = require('mongodb').ObjectID
 const User = require('./User');
 
-let Post = function (data, userid) {
+let Post = function (data, userid, requestedPostId) {
     this.data = data
     this.errors = []
     this.userid = userid
+    this.requestedPostId = requestedPostId
 }
 
 Post.prototype.cleanUp = function () {
@@ -51,6 +52,49 @@ Post.prototype.create = function () {
         }
     })
 }
+
+Post.prototype.update = function () {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let post = await Post.findSingleById(this.requestedPostId, this.userid)
+            if (post.isVisitorOwner) {
+                // update the db post
+                let status = await this.performUpdate()
+                resolve(status)
+            } else {
+                reject()
+            }
+
+        } catch {
+            reject()
+        }
+    })
+}
+
+Post.prototype.performUpdate = function () {
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            await postsCollection.findOneAndUpdate(
+                {
+                    _id: new ObjectID(this.requestedPostId)
+                },
+                {
+                    $set: {
+                        title: this.data.title,
+                        body: this.data.body
+                    }
+                }
+            )
+            resolve('success')
+        } else {
+            resolve('failure')
+        }
+
+    })
+}
+
 Post.reusablePostQuery = function (uniqueOperations, visitorId) {
     return new Promise(async function (resolve, reject) {
         let aggOperations = uniqueOperations.concat([
@@ -127,4 +171,5 @@ Post.findByAuthorId = function (authorId) {
         }
     ])
 }
+
 module.exports = Post
