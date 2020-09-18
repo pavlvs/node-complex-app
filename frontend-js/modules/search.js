02d9be857e3de22094ce0d708b075608cc22c684
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-09-18 13:20:11 +0100

    enabled following of users

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import DOMPurify from 'dompurify'
 export default class Search {
     // 1. select DOM elements
     constructor() {
@@ -62,7 +63,7 @@ export default class Search {
 
     renderResultsHTML(posts) {
         if (posts.length) {
-            this.resultsArea.innerHTML = `
+            this.resultsArea.innerHTML = DOMPurify.sanitize(`
             <div class="list-group shadow-sm">
                 <div class="list-group-item active"><strong>Search Results</strong> (${posts.length != 1 ? posts.length + ' items found' : ' 1 item found'})</div>
                 ${posts.map(post => {
@@ -75,7 +76,7 @@ export default class Search {
                     `
             }).join('')}
                 </div>
-            `
+            `)
         } else {
             this.resultsArea.innerHTML = `
             <p class="alert alert-danger text-center shadow-sm" >Sorry. No search results for that search term</p>
